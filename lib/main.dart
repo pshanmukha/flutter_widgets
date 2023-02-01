cde425fbe09991a9e891a040171add282503e87c
Shanmukha <55737606+pshanmukha@users.noreply.github.com>
2023-02-01 21:31:05 +0530

    added asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/material_banner_widget.dart';
+import 'package:flutter_widgets/material_banner/material_banner_widget.dart';
 
 void main() {
   runApp(const MyApp());
